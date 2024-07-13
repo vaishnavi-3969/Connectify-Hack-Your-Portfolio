6c3f3e4869c41fd6dd5b647fdb561968ffcf2a82
vaishnavi-3969 <vaishnavi.kale3011@gmail.com>
2024-07-14 01:31:03 +0530

    minor change: button to center(align)

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -5,15 +5,21 @@ import { motion } from 'framer-motion';
 const Landing = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-black py-7 bg-gradient-to-b from-mint-green to-light-blue">
-
-      <motion.div initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
+      <motion.div 
+        initial={{ opacity: 0, y: -50 }} 
+        animate={{ opacity: 1, y: 0 }} 
+        transition={{ duration: 0.5 }}
+        className="flex flex-col items-center"
+      >
         <h1 className="mb-8 text-5xl font-bold">Welcome to Connectify</h1>
         <p className="max-w-lg mb-12 text-lg text-center">
           Connectify is your platform for networking, mentorship, and project collaboration. Join us in building connections that matter.
         </p>
-          <Link to="/login" className="items-center justify-center px-6 py-3 text-lg font-semibold text-white duration-300 rounded-md text-transition-colors bg-dark-purple hover:bg-marian-blue">
+        <div className="flex justify-center mb-12">
+          <Link to="/login" className="items-center justify-center px-6 py-3 text-lg font-semibold text-center text-white duration-300 rounded-md text-transition-colors bg-dark-purple hover:bg-marian-blue">
             Sign Up/Login
           </Link>
+        </div>
         <div className="mt-12 text-center">
           <p className="mb-4 text-lg">Join Connectify to:</p>
           <ul className="text-lg list-disc list-inside">
